b9bb003c325ce5d3e974ca98326768969e2f2b60
Jay Barode <109953868+learnerjay784@users.noreply.github.com>
2022-10-28 23:13:12 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 27 14:11:44 2021
+
+@author: hp
+"""
+
+print('#'*50)
+print('Welcome to MY PATTERNS...')
+print('just call square,right_angle_triangle,pyramid,diamond functions to print shapes...')
+print('#'*50)
