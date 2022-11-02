971a2186f6708786be5d476f2c8e0991a129e473
Jay Barode <109953868+learnerjay784@users.noreply.github.com>
2022-11-02 23:13:35 +0530

    Add files via upload

---
+++ #381.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 15 16:33:50 2022
+
+@author: hp
+"""
+#380
+f=open('abc.txt','a')
+
+f.write('Durga\n')
+f.write('Software\n')
+f.write('Solutions\n')
+f.write('Hyderabad\n')
+
+f.close()
+print('Data written to the file successfully')
