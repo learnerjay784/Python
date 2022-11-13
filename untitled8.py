b92336630a1484211716dbcb90322d5958299789
Jay Barode <109953868+learnerjay784@users.noreply.github.com>
2022-11-13 21:24:21 +0530

    Add files via upload

---
+++ untitled8.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 13 17:30:45 2022
+
+@author: hp
+"""
+
+import logging
+logging.basicConfig(format='%(levelname)s')
+print('logging Demo')
+logging.critical('This is critical message')
+logging.error('This is error message')
+logging.warning('This is warning message')
+logging.info('This is info message')
+logging.debug('This is debug message')
